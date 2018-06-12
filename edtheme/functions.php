1dbaa2b1d932ba82245d9c382ca39ca46df3f3c4
Jonathan MirCha <jonmircha@gmail.com>
2018-06-11 19:44:15 -0500

    Integración de funciones avanzadas

--- edtheme/functions.php
+++ edtheme/functions.php
@@ -11,8 +11,12 @@
  */
 
 global $google_fonts;
+global $font_awesome;
+global $hamburgers;
 
 $google_fonts = 'https://fonts.googleapis.com/css?family=Raleway:400,700';
+$font_awesome = 'https://use.fontawesome.com/releases/v5.0.13/css/all.css';
+$hamburgers = 'https://cdnjs.cloudflare.com/ajax/libs/hamburgers/0.9.3/hamburgers.min.css';
 
 //https://codex.wordpress.org/Content_Width
 //Establecer el ancho máximo permitido para cualquier contenido en el tema, como oEmbeds e imágenes
@@ -23,9 +27,14 @@ if ( !isset( $content_width ) ) {
 if ( !function_exists('mawt_scripts') ):
   function mawt_scripts () {
     global $google_fonts;
+    global $font_awesome;
+    global $hamburgers;
 
     wp_enqueue_style( 'google-fonts', $google_fonts, array(), '1.0.0', 'all' );
-    wp_enqueue_style( 'style', get_stylesheet_uri(), array('google-fonts'), '1.0.0', 'all' );
+    wp_enqueue_style( 'font-awesome', $font_awesome, array(), '5.0.13', 'all' );
+    wp_enqueue_style( 'hamburgers', $hamburgers, array(), '0.9.3', 'all' );
+    wp_enqueue_style( 'custom-properties', get_template_directory_uri() . '/css/custom_properties.css', array('google-fonts'), '1.0.0', 'all' );
+    wp_enqueue_style( 'style', get_stylesheet_uri(), array('google-fonts', 'font-awesome', 'hamburgers', 'custom-properties'), '1.0.0', 'all' );
 
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'script', get_template_directory_uri() . '/script.js', array('jquery'), '1.0.0', true );
@@ -111,3 +120,15 @@ function mawt_setup () {
 }
 
 add_action('after_setup_theme', 'mawt_setup');
+
+require_once get_template_directory() . '/inc/custom-header.php';
+
+require_once get_template_directory() . '/inc/customizer.php';
+
+require_once get_template_directory() . '/inc/custom-excerpt.php';
+
+require_once get_template_directory() . '/inc/custom-description.php';
+
+require_once get_template_directory() . '/inc/custom-login.php';
+
+require_once get_template_directory() . '/inc/custom-admin.php';
--- edtheme/header.php
+++ edtheme/header.php
@@ -4,33 +4,17 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+  <meta name="description" content="<?php mawt_custom_meta_description(); ?>">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
     <header class="Header">
       <section class="Header-container">
-        <div class="Logo">
-          <a href="<?php echo esc_url( home_url( '/' )  );  ?>">Logo</a>
-        </div>
-        <button class="Panel-btn"><?php _e('Menú Principal', 'mawt');  ?></button>
-        <section class="Panel">
-          <?php
-            if ( has_nav_menu( 'main_menu' ) ):
-              wp_nav_menu( array(
-                'theme_location' => 'main_menu',
-                'container' => 'nav',
-                'container_class' => 'Menu'
-              ) );
-            else:
-          ?>
-              <nav class="Menu">
-                <ul>
-                  <?php wp_list_pages('title_li'); ?>
-                </ul>
-              </nav>
-          <?php endif; ?>
-        </section>
+        <?php
+        get_template_part( 'template-parts/header-logo' );
+        get_template_part( 'template-parts/header-menu' );
+        ?>
       </section>
     </header>
+    <?php   get_template_part( 'template-parts/header-custom' ); ?>
     <section class="Content">
